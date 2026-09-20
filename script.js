07ac958351f301aa8f3a5ba3d237e89c894722c6
Kyrexy <loldude.bm@gmail.com>
2026-09-20 14:17:39 +0700

    Made some changes to the portofolio section and removed some unnecessary code

--- script.js
+++ script.js
@@ -3,4 +3,5 @@ const navbar = document.querySelector('.navbar');
 
 menuIcon.onclick = () => {
     navbar.classList.toggle('active');
+    menuIcon.textContent = navbar.classList.contains('active') ? '\u2715' : '\u2630';
 };
